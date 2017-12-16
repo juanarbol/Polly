9be195e9335c937c12dd7f91ba1c0d052076b1d0
Juan José <soyjuanarbol@gmail.com>
2017-12-16 04:05:24 -0500

    Disable form when percentage is 100, fieldset implemented

--- app/components/notes/NotesInput.jsx
+++ app/components/notes/NotesInput.jsx
@@ -11,6 +11,8 @@ export default class NotesInput extends Component {
 
     this._handleSubmit = this._handleSubmit.bind(this)
     this._handleChangeOf = this._handleChangeOf.bind(this)
+    this._maxPercent = this._maxPercent.bind(this)
+    this._disableForm = this._disableForm.bind(this)
   }
 
   _handleSubmit (event) {
@@ -20,6 +22,7 @@ export default class NotesInput extends Component {
     let percentage = this.state.percentage
 
     this.props.onNewNote({ note, percentage })
+    this._maxPercent()
     this.setState({ note: '', percentage: '' })
   }
 
@@ -27,18 +30,40 @@ export default class NotesInput extends Component {
     this.setState({ [input]: newValue })
   }
 
+  _maxPercent () {
+    return 100 - this.props.currentPercent
+  }
+
+  _disableForm () {
+    let totalPercent = this.props.currentPercent
+    return totalPercent === 100
+  }
+
   render () {
     let note = this.state.note
     let percentage = this.state.percentage
     return (
       <form onSubmit={this._handleSubmit}>
-        <label>
-          Note:
-          <input type='number' value={note} onChange={event => this._handleChangeOf('note', event.target.value)} min='0' step='0.1' />
-          Percentage:
-          <input type='number' value={percentage} onChange={event => this._handleChangeOf('percentage', event.target.value)} min='0'step='1' />
-        </label>
-        <input type='submit' value='Submit' />
+        <fieldset disabled={this._disableForm()} >
+          <label>
+            Note:
+            <input type='number'
+              value={note}
+              onChange={event => this._handleChangeOf('note', event.target.value)}
+              min='0'
+              step='0.1'
+            />
+            Percentage:
+            <input type='number'
+              value={percentage}
+              onChange={event => this._handleChangeOf('percentage', event.target.value)}
+              max={this._maxPercent()}
+              min='0'
+              step='1'
+            />
+          </label>
+          <input type='submit' value='Submit' />
+        </fieldset>
       </form>
     )
   }
