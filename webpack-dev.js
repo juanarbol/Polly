03d9a52465761e547a7507d4c4aa03ed21bceacf
Juan José <juan.jose.arboleda.a@gmail.com>
2018-04-01 20:39:25 -0500

    Remove all root files

--- webpack-dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  contentBase: 'build/',
-  port: 9000
-}
--- webpack-prod.js
+++
@@ -1,5 +0,0 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
-module.exports = new UglifyJsPlugin({
-  test: /\.jsx?/
-})
